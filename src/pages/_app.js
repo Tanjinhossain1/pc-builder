518d63dc9e6352c38ed746f3408cafbd5efce5d7
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-07-27 23:17:04 +0600

    adding navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import TopNavbar from '@/components/Navbar';
 import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <TopNavbar />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
