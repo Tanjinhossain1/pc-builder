b79b84dc43e4e9fbc1a48d1e6c370ad4ae70cc29
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-07-30 11:50:27 +0600

    complte :

--- src/pages/categoryProducts/index.js
+++ src/pages/categoryProducts/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { server_url } from "@/components/Constant/constant";
 import FeaturedCategoryCard from "@/components/Featured/Category";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -38,6 +38,7 @@ export default function categoryPage({ products }) {
 
   return (
     <div style={{width: "80%", marginLeft: "auto", marginRight: "auto", marginTop: 2}}>
+        <Typography sx={{textAlign: "center", fontSize: 30, fontWeight: 600}}>{category}</Typography> 
       <Grid container item spacing={2}>
         {allProductDetail?.map((product, index) => (
           <Grid item key={index} gap={6} xs={12} sm={6} md={6}>
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,11 +1,12 @@
 import { GitHub, GiteOutlined } from "@mui/icons-material";
-import { Button, Grid, IconButton } from "@mui/material";
+import { Button, Grid, IconButton, Typography } from "@mui/material";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function LoginPage() {
   return (
     <>
       <br /> <br /> <br /> <br />  <br />{" "}
+            <Typography sx={{textAlign: "center", fontSize: 28, fontWeight: 600, mr: 5}}>Login Page</Typography>
       <br /> <br /> <br />
       <Grid item container>
         <Grid xs={5.5}></Grid>
